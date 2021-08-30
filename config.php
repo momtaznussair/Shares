73802f619a61d3debf3a636a70cf2b1983a84346
momtaz <momtaz_nussair@yahoo.com>
2021-08-30 14:47:00 +0200

    edited config file

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 // Define DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "123456");
+define("DB_USER", "");
+define("DB_PASS", "");
 define("DB_NAME", "shareboard");
 
 // Define URL
